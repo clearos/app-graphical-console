f0b0ed8aebb7007b91cb6eab586cb81b9768b6a2
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-12-06 19:28:28 +0000

    - Fixed cert8.db issue [tracker #871]

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'graphical_console';
-$app['version'] = '1.1.0';
+$app['version'] = '1.4.7';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
