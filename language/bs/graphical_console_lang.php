a55a311e7422fc97b672c4192a521dfe84d97c01
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 03:17:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/graphical_console_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['graphical_console_app_description'] = 'U Grafički konzola omogućuje administratorima konfigurirati osnovne postavke u grafičkom okruženju razumljiv';
+$lang['graphical_console_app_name'] = 'grafički Console';
+$lang['graphical_console_browser_warning_help'] = 'Kada povežete sa svojim web preglednik prvi put, vidjet ćete prikazati sigurnosno upozorenje. Molim te, ne boj se!';
+$lang['graphical_console_browser_warning'] = 'Web Browser Sigurnosno upozorenje';
+$lang['graphical_console_command_line'] = 'Command Line';
+$lang['graphical_console_command_line_help'] = 'Za one koji vole komandne linije, možete skočiti pravo u udarajući tipku izlaz.';
+$lang['graphical_console_configure_network_now'] = 'Konfigurirati Network Sada';
+$lang['graphical_console_follow_link_to_change_settings:'] = 'Molimo vas da slijedite link da promenite podešavanja:';
+$lang['graphical_console_ip_address:'] = 'IP adresa ovog sistema:';
+$lang['graphical_console_network_console'] = 'mreža konzole';
+$lang['graphical_console_no_ip_address_available'] = 'Izgleda da nemate IP adresu na raspolaganju za udaljene veze.';
+$lang['graphical_console_shutting_down_graphical_console'] = 'Isključivanje grafički konzole.';
+$lang['graphical_console_step_1_description'] = 'Korak 1. Konfigurirajte Vaš Network Settings';
+$lang['graphical_console_step_2_description'] = 'Korak 2. Spojite se sa svojim Web Browser';
+$lang['graphical_console_type_address_in_following_format:'] = 'Kada je vaša mreža konfigurirana, upišite adresu u sljedećem formatu:';
+$lang['graphical_console_type_following_address:'] = 'Pokrenite web preglednik i upišite sljedeće web adresi:';
+$lang['graphical_console_web_based_interface_access'] = 'sučelje web-based se može pristupiti na portu 81 koristeći siguran web protokol - HTTPS.';
+$lang['graphical_console_welcome_message'] = 'Dobrodošli! Ova konzola se koristi za konfiguriranje mrežne postavke na ovom sistemu. Kada imate mrežu gore i trčanje, možete instalirati, konfigurirati i upravljati aplikacijama pomoću standardnog web preglednika.';
+$lang['graphical_console_what_next_help'] = 'Kada ste podesili svoju mrežu i priključen na ovaj sistem sa svojim web preglednik, instalaciju čarobnjak će vas kroz korake završili instalaciju.';
+$lang['graphical_console_what_next'] = 'Šta dalje?';
