fd1e615c5cb8e81c1c8a2c6c6fe7f22d0b013152
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 03:18:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bg/graphical_console_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['graphical_console_app_description'] = 'Конзолата за Graphical позволява на администраторите да конфигурирате основни настройки в една лесна за ползване графична среда';
+$lang['graphical_console_app_name'] = 'Графично Console';
+$lang['graphical_console_browser_warning_help'] = 'Когато се свържете с вашия уеб браузър за първи път, ще видите предупреждение за сигурност показва. ';
+$lang['graphical_console_browser_warning'] = 'Уеб Предупреждение Browser Security';
+$lang['graphical_console_command_line'] = 'Command Line';
+$lang['graphical_console_command_line_help'] = 'За тези от вас, които предпочитат командния ред, можете да го направите веднага, като натиснете бутона за излизане.';
+$lang['graphical_console_configure_network_now'] = 'Configure Network Now';
+$lang['graphical_console_follow_link_to_change_settings:'] = 'Моля, последвайте линка, за да промените настройките си:';
+$lang['graphical_console_ip_address:'] = 'The IP address of this system:';
+$lang['graphical_console_network_console'] = 'Console Network';
+$lang['graphical_console_no_ip_address_available'] = 'Изглежда, че не е нужно да е на разположение за дистанционно свързване на IP адрес.';
+$lang['graphical_console_shutting_down_graphical_console'] = 'Изключване на графичната конзола.';
+$lang['graphical_console_step_1_description'] = ' Конфигурирате мрежовия Settings';
+$lang['graphical_console_step_2_description'] = ' Свържете се с вашия уеб браузър';
+$lang['graphical_console_type_address_in_following_format:'] = 'След като мрежата е конфигурирана, въведете адреса в следния формат:';
+$lang['graphical_console_type_following_address:'] = 'Започнете вашия уеб браузър и въведете следния интернет адрес:';
+$lang['graphical_console_web_based_interface_access'] = 'В уеб-базиран интерфейс може да бъде достъпен на порт 81, като се използва защитен уеб протокола - HTTPS.';
+$lang['graphical_console_welcome_message'] = 'Добре дошли! Тази конзола се използва за конфигуриране на мрежовите настройки на тази система. След като сте си мрежа и работи, можете да инсталирате, конфигурирате и управлявате приложения с помощта на стандартен уеб браузър.';
+$lang['graphical_console_what_next_help'] = 'След като сте конфигурирали вашата мрежа и свързани към тази система с вашия уеб браузър, съветникът за инсталиране ще ви преведе през стъпките завършите инсталацията.';
+$lang['graphical_console_what_next'] = 'Какво следва?';
