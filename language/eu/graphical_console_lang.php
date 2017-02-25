e2d5fab8448e2df3080171b90673f9227135dd4b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 03:17:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/graphical_console_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['graphical_console_app_description'] = 'The Graphical Console allows administrators to configure basic settings in a user-friendly graphical environment';
+$lang['graphical_console_app_name'] = 'grafikoa Console';
+$lang['graphical_console_browser_warning_help'] = 'Noiz konektatu dituzu zure nabigatzailean-rekin lehen aldiz, segurtasun abisu bat bistaratuko ikusiko duzu. Mesedez, ez kezkatu!';
+$lang['graphical_console_browser_warning'] = 'Web Browser segurtasuna Abisua';
+$lang['graphical_console_command_line'] = 'Komando lerroa';
+$lang['graphical_console_command_line_help'] = 'nahiago duten komando lerroan zaretenok, eskuineko salto egin ahal irteera botoia sakatuz.';
+$lang['graphical_console_configure_network_now'] = 'Konfiguratu Network orain';
+$lang['graphical_console_follow_link_to_change_settings:'] = 'Mesedez, jarraitu esteka zure ezarpenak aldatu ahal izateko:';
+$lang['graphical_console_ip_address:'] = 'IP sistema hau helbide:';
+$lang['graphical_console_network_console'] = 'Network Console';
+$lang['graphical_console_no_ip_address_available'] = 'Atsegin ez duzun IP helbide bat urruneko konexioak eskuragarri dirudi.';
+$lang['graphical_console_shutting_down_graphical_console'] = 'kontsola grafikoa itzaltzen.';
+$lang['graphical_console_step_1_description'] = 'Step 1. Konfiguratu sareko ezarpenak';
+$lang['graphical_console_step_2_description'] = 'Step 2. Lotu web arakatzailearekin';
+$lang['graphical_console_type_address_in_following_format:'] = 'Behin zure sarea konfiguratuta dago, idatzi honako formatuan helbidea:';
+$lang['graphical_console_type_following_address:'] = 'Hasi zure web nabigatzailea eta idatzi honako web helbidea:';
+$lang['graphical_console_web_based_interface_access'] = 'web interfazearen den 81 atakan sar daiteke segurtatu web protokoloa erabiliz - HTTPS.';
+$lang['graphical_console_welcome_message'] = 'Ongi etorri! Kontsola honek sistema honetan sarearen ezarpenak konfiguratzeko erabiltzen da. Behin zure sarea martxan, instalatu dezakezu, konfiguratzeko eta aplikazioak web nabigatzailea estandar bat erabiliz kudeatzeko.';
+$lang['graphical_console_what_next_help'] = 'Behin zure sarea konfiguratu duzula eta sistema honekin konektatutako zure web nabigatzailea, instalatu morroiak eramango zaitu urrats bidez instalazioa amaitzeko.';
+$lang['graphical_console_what_next'] = 'Zer da hurrengoa?';
