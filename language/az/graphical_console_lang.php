a7d36ea5ffe7ee66905f33736305d388e1d5956c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 03:17:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/graphical_console_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['graphical_console_app_description'] = 'The Graphical Console allows administrators to configure basic settings in a user-friendly graphical environment';
+$lang['graphical_console_app_name'] = 'Graphical Console';
+$lang['graphical_console_browser_warning_help'] = ' ';
+$lang['graphical_console_browser_warning'] = 'Web Browser Security Warning';
+$lang['graphical_console_command_line'] = 'Command Line';
+$lang['graphical_console_command_line_help'] = 'For those of you who prefer the command line, you can jump right in by hitting the exit button.';
+$lang['graphical_console_configure_network_now'] = 'Network İndi konfiqurasiya';
+$lang['graphical_console_follow_link_to_change_settings:'] = 'Please follow the link to change your settings:';
+$lang['graphical_console_ip_address:'] = 'The IP address of this system:';
+$lang['graphical_console_network_console'] = 'Network Console';
+$lang['graphical_console_no_ip_address_available'] = 'It looks like you do not have an IP address available for remote connections.';
+$lang['graphical_console_shutting_down_graphical_console'] = 'qrafik konsol bağlanılır.';
+$lang['graphical_console_step_1_description'] = ' ';
+$lang['graphical_console_step_2_description'] = ' ';
+$lang['graphical_console_type_address_in_following_format:'] = 'Şəbəkə konfiqurasiya sonra, aşağıdakı formatda ünvanını yazın:';
+$lang['graphical_console_type_following_address:'] = 'Start your web browser and type the following web address:';
+$lang['graphical_console_web_based_interface_access'] = 'The web-based interface can be accessed on port 81 using the secure web protocol - HTTPS.';
+$lang['graphical_console_welcome_message'] = '  Siz şəbəkə və çalışan sonra, yüklemek konfiqurasiya və standart web browser istifadə edərək apps idarə edə bilərsiniz.';
+$lang['graphical_console_what_next_help'] = 'Once you have configured your network and connected to this system with your web browser, the install wizard will take you through the steps finish the installation.';
+$lang['graphical_console_what_next'] = 'What Next?';
