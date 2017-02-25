88639385314671bc223f9f360d646459d98a4fa1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 03:17:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/be/graphical_console_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['graphical_console_app_description'] = 'Графічная кансоль дазваляе адміністратарам наладжваць базавыя параметры ў зручным для карыстальніка графічным асяроддзі';
+$lang['graphical_console_app_name'] = 'графічная кансоль';
+$lang['graphical_console_browser_warning_help'] = 'Падчас падлучэння з дапамогай вэб-браўзэра ў першы раз, вы ўбачыце папярэджанне аб бяспецы адлюстроўваецца. Калі ласка, не хвалюйцеся!';
+$lang['graphical_console_browser_warning'] = 'Вэб-браўзэр Папярэджанне бяспекі';
+$lang['graphical_console_command_line'] = 'камандны радок';
+$lang['graphical_console_command_line_help'] = 'Для тых з вас, хто аддае перавагу камандную радок, вы можаце адразу перайсці, націснуўшы на кнопку выхаду.';
+$lang['graphical_console_configure_network_now'] = 'Налада Цяпер сетка';
+$lang['graphical_console_follow_link_to_change_settings:'] = 'Калі ласка, перайдзіце па спасылцы, каб змяніць налады:';
+$lang['graphical_console_ip_address:'] = 'IP-адрас гэтай сістэмы:';
+$lang['graphical_console_network_console'] = 'сеткавая кансоль';
+$lang['graphical_console_no_ip_address_available'] = 'Падобна на тое, што вы не маеце IP-адрас, даступны для аддаленага падключэння.';
+$lang['graphical_console_shutting_down_graphical_console'] = 'Выключэнне графічнай кансолі.';
+$lang['graphical_console_step_1_description'] = 'Крок 1. Налада сеткавых параметраў';
+$lang['graphical_console_step_2_description'] = 'Крок 2. Падключэнне з дапамогай вэб-браўзэра';
+$lang['graphical_console_type_address_in_following_format:'] = 'Пасля таго, як сетка настроена, увядзіце адрас у наступным фармаце:';
+$lang['graphical_console_type_following_address:'] = 'Запусціце вэб-браўзэр і ўвядзіце наступны вэб-адрас:';
+$lang['graphical_console_web_based_interface_access'] = 'Вэб-інтэрфейс можна атрымаць доступ на порт 81 з выкарыстаннем абароненага вэб-пратаколу - HTTPS.';
+$lang['graphical_console_welcome_message'] = 'Сардэчна запрашаем! Гэтая кансоль выкарыстоўваецца для налады сеткавых параметраў у гэтай сістэме. Пасля таго, як у вас ёсць сеткі і працуе, вы можаце ўсталяваць, наладзіць і кіраваць праграмамі, выкарыстоўваючы стандартны вэб-браўзэр.';
+$lang['graphical_console_what_next_help'] = 'Пасля таго, як вы наладзілі сваю сетку і падлучаныя да гэтай сістэмы з дапамогай вэб-браўзэра, майстар ўстаноўкі правядзе вас праз крокі скончыць ўстаноўку.';
+$lang['graphical_console_what_next'] = 'Што далей?';
