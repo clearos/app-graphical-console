9890ff6aaba45c822cba641dc6795300bbed241b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 03:16:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/graphical_console_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['graphical_console_app_description'] = 'The Graphical Console allows administrators to configure basic settings in a user-friendly graphical environment';
+$lang['graphical_console_app_name'] = 'Graphical Console';
+$lang['graphical_console_browser_warning_help'] = ' ';
+$lang['graphical_console_browser_warning'] = 'Web Browser Security Warning';
+$lang['graphical_console_command_line'] = 'Command Line';
+$lang['graphical_console_command_line_help'] = 'Vir dié van julle wat verkies om die command line, kan jy reg in te spring deur slaan die uitgang knoppie.';
+$lang['graphical_console_configure_network_now'] = 'Configure Network Now';
+$lang['graphical_console_follow_link_to_change_settings:'] = 'Please follow the link to change your settings:';
+$lang['graphical_console_ip_address:'] = 'Die IP-adres van die stelsel:';
+$lang['graphical_console_network_console'] = 'Network Console';
+$lang['graphical_console_no_ip_address_available'] = 'It looks like you do not have an IP address available for remote connections.';
+$lang['graphical_console_shutting_down_graphical_console'] = 'Shutting down graphical console.';
+$lang['graphical_console_step_1_description'] = ' Stel jou Netwerk Stellings';
+$lang['graphical_console_step_2_description'] = ' ';
+$lang['graphical_console_type_address_in_following_format:'] = 'Once your network is configured, type the address in the following format:';
+$lang['graphical_console_type_following_address:'] = 'Start your web browser and type the following web address:';
+$lang['graphical_console_web_based_interface_access'] = 'Die web-gebaseerde koppelvlak kan verkry word op poort 81 met behulp van die veilige web-protokol - HTTPS.';
+$lang['graphical_console_welcome_message'] = '  ';
+$lang['graphical_console_what_next_help'] = 'Once you have configured your network and connected to this system with your web browser, the install wizard will take you through the steps finish the installation.';
+$lang['graphical_console_what_next'] = 'Wat volgende?';
