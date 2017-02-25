6421387d6fcf334ed00475f1d3760a8644cf2da7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 03:17:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/graphical_console_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['graphical_console_app_description'] = 'গ্রাফিকাল কনসোল প্রশাসকদের একটি ব্যবহারকারী বান্ধব গ্রাফিক্যাল পরিবেশে মৌলিক সেটিংস কনফিগার করতে পারেন';
+$lang['graphical_console_app_name'] = 'গ্রাফিকাল কনসোল';
+$lang['graphical_console_browser_warning_help'] = 'যখন আপনি আপনার ওয়েব ব্রাউজার দিয়ে প্রথমবার সংযোগ, আপনি একটি নিরাপত্তা সতর্কতা প্রদর্শিত দেখতে পাবেন. দয়া করে জ্বালাতন করবেন না!';
+$lang['graphical_console_browser_warning'] = 'ওয়েব ব্রাউজার নিরাপত্তা সতর্কীকরণ';
+$lang['graphical_console_command_line'] = 'কমান্ড লাইন থেকে ব্যবহারযোগ্য';
+$lang['graphical_console_command_line_help'] = 'তোমাদের মধ্যে যারা কমান্ড লাইন পছন্দ জন্য, আপনি প্রস্থান বাটন চাপার দ্বারা ডানদিকে তিড়িং লাফ পারেন.';
+$lang['graphical_console_configure_network_now'] = 'কনফিগার নেটওয়ার্ক এখন';
+$lang['graphical_console_follow_link_to_change_settings:'] = 'আপনার সেটিংস পরিবর্তন করতে লিঙ্কটি অনুসরণ করুন:';
+$lang['graphical_console_ip_address:'] = 'এই সিস্টেমের IP ঠিকানা:';
+$lang['graphical_console_network_console'] = 'নেটওয়ার্ক কনসোল';
+$lang['graphical_console_no_ip_address_available'] = 'দেখে মনে হচ্ছে আপনি একটি আইপি দূরবর্তী সংযোগ জন্য উপলব্ধ ঠিকানা নেই.';
+$lang['graphical_console_shutting_down_graphical_console'] = 'গ্রাফিকাল কনসোল শাট ডাউন.';
+$lang['graphical_console_step_1_description'] = 'ধাপ 1. আপনার নেটওয়ার্ক সেটিংস কনফিগার করুন Name';
+$lang['graphical_console_step_2_description'] = 'ধাপ ২. আপনার ওয়েব ব্রাউজারের সাথে সংযোগ স্থাপন করুন';
+$lang['graphical_console_type_address_in_following_format:'] = 'একবার আপনার নেটওয়ার্ক কনফিগার করা, নিম্নলিখিত বিন্যাসে ঠিকানা টাইপ করুন:';
+$lang['graphical_console_type_following_address:'] = 'আপনার ওয়েব ব্রাউজার আরম্ভ হয় এবং ওয়েব ঠিকানা টাইপ করুন:';
+$lang['graphical_console_web_based_interface_access'] = 'মধ্যে HTTPS - ওয়েব ভিত্তিক ইন্টারফেস নিরাপদ ওয়েব প্রোটোকল ব্যবহার পোর্ট 81 তে প্রবেশ করা যেতে পারে.';
+$lang['graphical_console_welcome_message'] = 'স্বাগত! এই কনসোল এই সিস্টেমে নেটওয়ার্ক সেটিংস কনফিগার করার জন্য ব্যবহার করা হয়. একবার আপনি আপনার নেটওয়ার্ক আপ এবং চলমান আছে, আপনি, ইনস্টল কনফিগার এবং একটি প্রমিত ওয়েব ব্রাউজার ব্যবহার করে অ্যাপ্লিকেশন পরিচালনা করতে পারেন.';
+$lang['graphical_console_what_next_help'] = 'একবার আপনি আপনার নেটওয়ার্ক কনফিগার করা এবং আপনার ওয়েব ব্রাউজার দিয়ে এই সিস্টেমের সাথে সংযুক্ত আছে, ইনস্টল উইজার্ড আপনি নিতে মাধ্যমে পদক্ষেপ ইনস্টলেশন প্রক্রিয়া সমাপ্ত হবে.';
+$lang['graphical_console_what_next'] = 'এরপর কী?';
