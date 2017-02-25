ecb150f273fcde95b0bea97da1a392ac3b3f1811
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 03:18:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/graphical_console_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['graphical_console_app_description'] = 'အဆိုပါ Graphical Console အအုပ်ချုပ်ရေးမှူးများအသုံးပြုသူတစ်ဦး-ဖော်ရွေသော graphical ပတ်ဝန်းကျင်အခြေခံ settings တွေကို configure လုပ်ဖို့ခွင့်ပြု';
+$lang['graphical_console_app_name'] = 'graphical Console';
+$lang['graphical_console_browser_warning_help'] = 'သင်သည်သင်၏ဝဘ်ဘရောက်ဆာနှင့်အတူပထမဦးဆုံးအကြိမ်အဖြစ်နဲ့ချိတ်ဆက်တဲ့အခါမှာ, သင်ကလုံခြုံရေးသတိပေးခကျြကိုပြသမြင်ရပါလိမ့်မည်။ ဖဲ့ခြွေကြပါဘူးကျေးဇူးပြုပြီး!';
+$lang['graphical_console_browser_warning'] = 'Web Browser Security Warning';
+$lang['graphical_console_command_line'] = 'command Line';
+$lang['graphical_console_command_line_help'] = 'ထို command line ကိုပိုနှစ်သက်သူအသငျသညျမြားအဘို့အသင်ထွက်ပေါက်ခလုတ်ကိုနှိပ်ခြင်းဖြင့်၌တရားခုန်နိုင်ပါတယ်။';
+$lang['graphical_console_configure_network_now'] = 'ယခုကွန်ယက်ကို configure';
+$lang['graphical_console_follow_link_to_change_settings:'] = 'Please follow the link to change your settings:';
+$lang['graphical_console_ip_address:'] = 'ဒီစနစ်ရဲ့ IP လိပ်စာ:';
+$lang['graphical_console_network_console'] = 'Network ကို Console';
+$lang['graphical_console_no_ip_address_available'] = 'သငျသညျဝေးလံခေါင်သီဆက်သွယ်မှုများအတွက်ရရှိနိုင်သည့် IP address ကိုရှိသည်မဟုတ်ကြဘူးနဲ့တူပုံရသည်။';
+$lang['graphical_console_shutting_down_graphical_console'] = 'သော graphical console ကိုပိတ်ပစ်။';
+$lang['graphical_console_step_1_description'] = 'အဆင့် 1 ။ သင့်ရဲ့ Network Settings များကို Configure';
+$lang['graphical_console_step_2_description'] = 'အဆင့် 2 ။ သင့်ရဲ့ Web Browser နှင့်အတူချိတ်ဆက်ပါ';
+$lang['graphical_console_type_address_in_following_format:'] = 'သင့် network configure လုပ်ပြီးတာနဲ့အောက်ပါ format နဲ့လိပ်စာကိုရိုက်ထည့်ပါ:';
+$lang['graphical_console_type_following_address:'] = 'Start your web browser and type the following web address:';
+$lang['graphical_console_web_based_interface_access'] = 'The web-based interface can be accessed on port 81 using the secure web protocol - HTTPS.';
+$lang['graphical_console_welcome_message'] = '  ';
+$lang['graphical_console_what_next_help'] = 'Once you have configured your network and connected to this system with your web browser, the install wizard will take you through the steps finish the installation.';
+$lang['graphical_console_what_next'] = 'What Next?';
