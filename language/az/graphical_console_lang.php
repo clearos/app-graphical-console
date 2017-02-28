e07f46978a64a504fa909b56d984aae6854cb697
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 23:49:31 +0000

    Add new files and directories and commit changes to local repository

--- language/az/graphical_console_lang.php
+++ language/az/graphical_console_lang.php
@@ -1,22 +1,22 @@
 <?php
 
-$lang['graphical_console_app_description'] = 'The Graphical Console allows administrators to configure basic settings in a user-friendly graphical environment';
-$lang['graphical_console_app_name'] = 'Graphical Console';
-$lang['graphical_console_browser_warning_help'] = ' ';
-$lang['graphical_console_browser_warning'] = 'Web Browser Security Warning';
+$lang['graphical_console_app_description'] = 'Qrafik Console idarəçilər bir istifadəçi dostu qrafik mühit əsas parametrləri konfiqurasiya imkan verir';
+$lang['graphical_console_app_name'] = 'Qrafik Console';
+$lang['graphical_console_browser_warning_help'] = 'Siz web browser ilə ilk dəfə əlaqə, bir təhlükəsizlik xəbərdarlıq göstərilir görəcəksiniz. lad etməyin!';
+$lang['graphical_console_browser_warning'] = 'Web Browser Təhlükəsizlik Warning';
 $lang['graphical_console_command_line'] = 'Command Line';
-$lang['graphical_console_command_line_help'] = 'For those of you who prefer the command line, you can jump right in by hitting the exit button.';
+$lang['graphical_console_command_line_help'] = 'command line üstünlük verən sizin üçün, exit düyməsinə basaraq doğru atlayabilir.';
 $lang['graphical_console_configure_network_now'] = 'Network İndi konfiqurasiya';
-$lang['graphical_console_follow_link_to_change_settings:'] = 'Please follow the link to change your settings:';
-$lang['graphical_console_ip_address:'] = 'The IP address of this system:';
+$lang['graphical_console_follow_link_to_change_settings:'] = 'ayarlarınızı dəyişdirmək üçün link edin:';
+$lang['graphical_console_ip_address:'] = 'Bu sistemin IP ünvanı:';
 $lang['graphical_console_network_console'] = 'Network Console';
-$lang['graphical_console_no_ip_address_available'] = 'It looks like you do not have an IP address available for remote connections.';
+$lang['graphical_console_no_ip_address_available'] = 'uzaq əlaqələri üçün mövcud bir IP ünvanı yoxdur kimi görünür.';
 $lang['graphical_console_shutting_down_graphical_console'] = 'qrafik konsol bağlanılır.';
-$lang['graphical_console_step_1_description'] = ' ';
-$lang['graphical_console_step_2_description'] = ' ';
+$lang['graphical_console_step_1_description'] = 'Addım 1. Sizin Network Settings konfiqurasiya';
+$lang['graphical_console_step_2_description'] = 'Addım 2. Web Browser ilə əlaqə';
 $lang['graphical_console_type_address_in_following_format:'] = 'Şəbəkə konfiqurasiya sonra, aşağıdakı formatda ünvanını yazın:';
-$lang['graphical_console_type_following_address:'] = 'Start your web browser and type the following web address:';
-$lang['graphical_console_web_based_interface_access'] = 'The web-based interface can be accessed on port 81 using the secure web protocol - HTTPS.';
-$lang['graphical_console_welcome_message'] = '  Siz şəbəkə və çalışan sonra, yüklemek konfiqurasiya və standart web browser istifadə edərək apps idarə edə bilərsiniz.';
-$lang['graphical_console_what_next_help'] = 'Once you have configured your network and connected to this system with your web browser, the install wizard will take you through the steps finish the installation.';
-$lang['graphical_console_what_next'] = 'What Next?';
+$lang['graphical_console_type_following_address:'] = 'Sizin web browser başlamaq və aşağıdakı web ünvanı yazın:';
+$lang['graphical_console_web_based_interface_access'] = 'HTTPS - web-based interface təhlükəsiz web protokolu istifadə port 81-də əldə edilə bilər.';
+$lang['graphical_console_welcome_message'] = 'Xoş gəlmisiniz! Bu konsol bu sistem şəbəkə parametrləri konfiqurasiya üçün istifadə olunur. Siz şəbəkə və çalışan sonra, yüklemek konfiqurasiya və standart web browser istifadə edərək apps idarə edə bilərsiniz.';
+$lang['graphical_console_what_next_help'] = 'Siz şəbəkə konfiqurasiya və web browser ilə bu sistemə qoşulub sonra addımlar quraşdırılması başa vasitəsilə yüklemek sehrbazdır olacaq.';
+$lang['graphical_console_what_next'] = 'Nə Next?';
